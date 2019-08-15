60407c9b35fe2896acade5ce5938354bc36799a6
Sara Harvey <saraharveynyc@gmail.com>
2019-08-15 15:02:50 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
 class Puppy 
-  attra_accessor :name, :breed, :months_old
+  
+  attr_accessor :name, :breed, :months_old
   
   def initialize(name, breed, months_old)
-    @name = name
-    @breed = breed
+    @name = name 
+    @breed = breed 
     @months_old = months_old
   end
-  
 end
