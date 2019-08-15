622fd2344ad017c086ebab8df3bb35903c3e8101
Sara Harvey <saraharveynyc@gmail.com>
2019-08-15 14:50:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,5 +3,9 @@ require_relative 'config/environment'
 #Implement a POST request to the controller to display data from a user in the view
 
 class App < Sinatra::Base
+  
+    get '/' do 
+    erb :index
+  end
 
 end
